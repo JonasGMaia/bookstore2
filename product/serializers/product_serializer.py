038b8a41387867eb6eb3cb8fe7a5f181aee915b9
Jonas Maia <jonasgmaia2@gmail.com>
2026-03-31 16:10:57 -0300

    tests corrections 1

--- product/serializers/product_serializer.py
+++ product/serializers/product_serializer.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 
 from product.models.product import Product
+from product.models.category import Category
 from product.serializers.category_serializer import CategorySerializer
 
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer(read_only=True,many=True)
-    category_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, write_only=True)
+    categories_id = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True, write_only=True)
 
     class Meta:
         model = Product

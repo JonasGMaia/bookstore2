3c95db0533b51f6c765bd73b9d8e9a6be47a24f8
Jonas Maia <jonasgmaia2@gmail.com>
2026-03-31 17:04:30 -0300

    add tests category

---
+++ product/tests/test_viewsets/test_category_viewset.py
@@ -0,0 +1,51 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
+
+from django.urls import reverse
+
+from product.factories import CategoryFactory, ProductFactory
+from order.factories import OrderFactory, UserFactory
+from order.models import Order
+from product.models import Category
+
+
+class TestCategoryViewSet(APITestCase):
+    client = APIClient()
+
+    def setUp(self):
+
+        self.category = CategoryFactory(title='books')
+        # self.product = ProductFactory(
+        #     title='pro controller',
+        #     price=200.00,
+        # )
+
+    def test_get_all_category(self):
+        response = self.client.get(
+            reverse('category-list', kwargs={'version': 'v1'})
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        category_data = json.loads(response.content)
+
+        self.assertEqual(category_data[0]['title'], self.category.title)
+
+    def test_create_category(self):
+        data = json.dumps({
+            'title': 'electronics'
+        })
+
+        response = self.client.post(
+            reverse('category-list', kwargs={'version': 'v1'}),
+            data=data,
+            content_type='application/json'
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        created_category = Category.objects.get(title='electronics')
+
+
+        self.assertEqual(created_category.title, 'electronics')
